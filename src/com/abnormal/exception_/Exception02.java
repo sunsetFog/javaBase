23166986607c8cbcb3c7f4a3a76f68a25da96ac5
sunsetFog <2693281934@qq.com>
2022-08-07 00:49:42 +0800

    异常处理0029

---
+++ src/com/abnormal/exception_/Exception02.java
@@ -0,0 +1,26 @@
+package com.abnormal.exception_;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @author 韩顺平
+ * @version 1.0
+ */
+public class Exception02 {
+    public static void main(String[] args) {
+
+        try {
+            FileInputStream fis;
+            fis = new FileInputStream("D:\\upload\\1.png");
+            int len;
+            while ((len = fis.read()) != -1) {
+                System.out.println("---"+len);
+            }
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}

632b14c0eb23d4cbaf149fb7b6fa485994cf022f
sunsetFog <2693281934@qq.com>
2022-08-18 11:15:30 +0800

    线程0202

---
+++ src/com/xiancheng/syn/SellTicket.java
@@ -0,0 +1,158 @@
+package com.xiancheng.syn;
+
+/*
+    使用多线程，模拟三个窗口同时售票100张
+    同步方法
+ */
+public class SellTicket {
+    public static void main(String[] args) {
+
+        //测试
+//        SellTicket01 sellTicket01 = new SellTicket01();
+//        SellTicket01 sellTicket02 = new SellTicket01();
+//        SellTicket01 sellTicket03 = new SellTicket01();
+//
+//        //这里我们会出现超卖..
+//        sellTicket01.start();//启动售票线程
+//        sellTicket02.start();//启动售票线程
+//        sellTicket03.start();//启动售票线程
+
+
+//        System.out.println("===使用实现接口方式来售票=====");
+//        SellTicket02 sellTicket02 = new SellTicket02();
+//
+//        new Thread(sellTicket02).start();//第1个线程-窗口
+//        new Thread(sellTicket02).start();//第2个线程-窗口
+//        new Thread(sellTicket02).start();//第3个线程-窗口
+
+        //测试一把
+        SellTicket03 sellTicket03 = new SellTicket03();
+        new Thread(sellTicket03).start();//第1个线程-窗口
+        new Thread(sellTicket03).start();//第2个线程-窗口
+        new Thread(sellTicket03).start();//第3个线程-窗口
+
+    }
+}
+
+// ------------------------------------------------------------------------------------
+//实现接口方式, 使用synchronized实现线程同步
+class SellTicket03 implements Runnable {
+    private int ticketNum = 100;//让多个线程共享 ticketNum
+    private boolean loop = true;//控制run方法变量
+    Object object = new Object();
+
+    /*
+    静态方法中，实现一个同步代码块.
+        同步方法（静态的）的锁为当前类本身
+        synchronized (SellTicket03.class) {
+            System.out.println("m2");
+        }
+     */
+    public synchronized static void m1() {
+
+    }
+    public static  void m2() {// 静态
+        synchronized (SellTicket03.class) {// 锁：类本身
+            System.out.println("m2");
+        }
+    }
+
+    /*
+     普通方法中，实现一个同步代码块.
+        public synchronized void sell() {} 就是一个同步方法
+    */
+    public /*synchronized*/ void sell() { //同步方法, 在同一时刻， 只能有一个线程来执行sell方法
+
+        synchronized (/*this*/ object) {// 互斥锁是this或object
+            if (ticketNum <= 0) {
+                System.out.println("售票结束...");
+                loop = false;
+                return;
+            }
+
+            //休眠50毫秒, 模拟
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("窗口 " + Thread.currentThread().getName() + " 售出一张票"
+                    + " 剩余票数=" + (--ticketNum));//1 - 0 - -1  - -2
+        }
+    }
+
+    @Override
+    public void run() {
+        while (loop) {
+
+            sell();//sell方法是一共同步方法
+        }
+    }
+}
+// ------------------------------------------------------------------------------------
+//使用Thread方式，不能用。。。
+// new SellTicket01().start()
+// new SellTicket01().start();
+class SellTicket01 extends Thread {
+
+    private static int ticketNum = 100;//让多个线程共享 ticketNum
+
+//    public void m1() {
+//        synchronized (this) {// 这里不能用this当锁，线程this不同
+//            System.out.println("hello");
+//        }
+//    }
+
+    @Override
+    public void run() {
+
+
+        while (true) {
+
+            if (ticketNum <= 0) {
+                System.out.println("售票结束...");
+                break;
+            }
+
+            //休眠50毫秒, 模拟
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("窗口 " + Thread.currentThread().getName() + " 售出一张票"
+                    + " 剩余票数=" + (--ticketNum));
+
+        }
+    }
+}
+
+// ------------------------------------------------------------------------------------
+//实现接口方式
+class SellTicket02 implements Runnable {
+    private int ticketNum = 100;//让多个线程共享 ticketNum
+
+    @Override
+    public void run() {
+        while (true) {
+
+            if (ticketNum <= 0) {
+                System.out.println("售票结束...");
+                break;
+            }
+
+            //休眠50毫秒, 模拟
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("窗口 " + Thread.currentThread().getName() + " 售出一张票"
+                    + " 剩余票数=" + (--ticketNum));//1 - 0 - -1  - -2
+
+        }
+    }
+}
